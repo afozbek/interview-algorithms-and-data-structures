b00640a2ce37c5b5fa34a8bbc1a7dfe950036bb7
afozbek <furkanozbek1995@gmail.com>
2018-11-29 23:53:33 +0300

    End_Reverse_String

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,25 +6,31 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-
-//Possible solution return str.split('').reverse().join('') 
 function reverse(str) {
-    return str.split('').reduce((rev, char) => char + rev, '')
+    let reversed = '';
+    for (let character of str) {
+        reversed = character + reversed;
+    }
+    return reversed;
 }
+
 
 module.exports = reverse;
 
-//Not Recommended!!! 
+//Not Recommended!!!
+// 1-
 // let result = []
 // for (let i = str.length; i >= 0; i--) {
 //     result.push(str[i]);
 // }
 // //With join you can return an string array to a string
 // return result.join('');
+//2-
 
-// let reversed = '';
 
-// for (let character of str) {
-//     reversed = character + reversed;
+//3-
+// return str.split('').reverse().join('') 
+//4-
+// function reverse(str) {
+//     return str.split('').reduce((rev, char) => char + rev, '')
 // }
-// return reversed;
