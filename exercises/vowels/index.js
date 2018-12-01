378103f2b395a6e3dcd3b8b98371514a144b066d
afozbek <furkanozbek1995@gmail.com>
2018-12-01 14:08:31 +0300

    vowels_finish

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,38 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
-
+function vowels(str) {
+    // /[matched characters]/g-->non stop i-->case like upperCase
+    const arr = str.match(/[aeiou]/gi)
+    return arr ? arr.length : 0;
+}
 module.exports = vowels;
+
+//My solution
+// function vowels(str) {
+//     let count = 0;
+//     for (let char of str.toLowerCase()) {
+//         if (isVowel(char)) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// function isVowel(ch) {
+//     if (ch == 'a' || ch == 'e' || ch == 'o' || ch == 'u' || ch == 'i') {
+//         return true;
+//     }
+//     return false;
+// }
+
+//1-
+// function vowels(str) {
+//     let count = 0;
+//     let _vowels = ['a', 'e', 'i', 'o', 'u'];
+//     for (let char of str.toLowerCase()) {
+//         if (_vowels.includes(char)) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
