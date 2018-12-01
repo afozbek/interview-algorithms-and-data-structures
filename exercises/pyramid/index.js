0dc074b05b184c9db151d8e4a9e7cd6378ab6bee
afozbek <furkanozbek1995@gmail.com>
2018-12-01 13:35:34 +0300

    vowels_problem

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,20 +14,24 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
+function pyramid(n, row = 0, column = 0, stair = '') {
     const mid = Math.floor((2 * n - 1) / 2);
-    for (let i = 0; i < n; i++) {
-        let str = '';
-        for (let j = 0; j < 2 * n - 1; j++) {
-            if (mid - i <= j && mid + i >= j) {
-                str += '#';
-            }
-            else {
-                str += ' ';
-            }
-        }
-        console.log(str);
+    if (n === row) {
+        return;
     }
+    if (stair.length === 2 * n - 1) {
+        console.log(stair);
+        return pyramid(n, row + 1);
+    }
+    // const add = stair.length <= row ? '#' : ' ';
+    //We can use stair.length instead column but I think it is more clear now!
+    if (mid - row <= column && mid + row >= column) {
+        stair += '#';
+    }
+    else {
+        stair += ' ';
+    }
+    pyramid(n, row, column + 1, stair);
 }
 module.exports = pyramid;
 
@@ -36,10 +40,43 @@ module.exports = pyramid;
 //     const str = '#'; const space = ' ';
 //     let totalStr = '';
 //     for (let row = 0; row < n; row++) {
-//         totalStr =
-//             space.repeat(n - row - 1) +
-//             str.repeat(2 * row + 1) +
-//             space.repeat(n - row - 1);
+//         let copyStr = space.repeat(n - row - 1);
+//         totalStr = copyStr + str.repeat(2 * row + 1) + copyStr;
 //         console.log(totalStr);
+//     }
+// }
+
+//MySol2-Recursive
+// function pyramid(n, row = 0, column = 0, stair = '') {
+//     const mid = Math.floor((2 * n - 1) / 2);
+//     if (n === row) {
+//         return;
+//     }
+//     if (stair.length === 2 * n - 1) {
+//         console.log(stair);
+//         return pyramid(n, row + 1);
+//     }
+//     // const add = stair.length <= row ? '#' : ' ';
+//     if (mid - row <= column && mid + row >= column) {
+//         stair += '#';
+//     }
+//     else {
+//         stair += ' ';
+//     }
+//     pyramid(n, row, column + 1, stair);
+// }
+// function pyramid(n) {
+//     const mid = Math.floor((2 * n - 1) / 2);
+//     for (let i = 0; i < n; i++) {
+//         let str = '';
+//         for (let j = 0; j < 2 * n - 1; j++) {
+//             if (mid - i <= j && mid + i >= j) {
+//                 str += '#';
+//             }
+//             else {
+//                 str += ' ';
+//             }
+//         }
+//         console.log(str);
 //     }
 // }
