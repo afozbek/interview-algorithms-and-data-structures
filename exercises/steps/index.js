2f6baf4206b25a5d92790667be79961cf4a00332
afozbek <furkanozbek1995@gmail.com>
2018-12-01 10:27:36 +0300

    end_of_steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,39 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
-
+function steps(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
+    }
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+    const add = stair.length <= row ? '#' : ' ';
+    steps(n, row, stair + add);
+}
 module.exports = steps;
+//My Sol
+//str.repeat(count)-->repeat the string by count
+// function steps(n) {
+//     const str = '#'; const space = ' ';
+//     let index = 1;
+//     for (let i = 1; i <= n; i++) {
+//         console.log(str.repeat(index) + space.repeat(n - index));
+//         index++;
+//     }
+
+// }
+//2-
+// function steps(n) {
+//     for (let i = 0; i < n; i++) {
+//         let stair = '';
+//         for (let j = 0; j < n; j++) {
+//             if (j <= i) {
+//                 stair += '#'
+//             }
+//             else stair += ' '
+//         }
+//         console.log(stair);
+//     }
+// }
