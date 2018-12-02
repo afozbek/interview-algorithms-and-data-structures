15a8f20a7fbc7083f97bb06220b79c0c6272592e
afozbek <furkanozbek1995@gmail.com>
2018-12-02 19:48:22 +0300

    size&getFirst_LinkedList

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,6 +2,12 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
+/*
+*
+* @author:Furkan Özbek
+*
+*/
+
 class Node {
     constructor(data, next = null) {
         this.data = data;
@@ -14,9 +20,22 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
-    //Data can be anythins an array, object or variable
+    //Data can be anything: an array, an object or a variable
     insertFirst(data) {
         this.head = new Node(data, this.head);
+    }
+    size() {
+        let count = 0;
+        //this.head-->holds the current node we are working or null
+        let node = this.head;
+        while (node) {
+            node = node.next;
+            count++;
+        }
+        return count;
+    }
+    getFirst() {
+        return this.head;
     }
 }
 
