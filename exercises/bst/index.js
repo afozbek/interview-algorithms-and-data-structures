c81c2dc1c93667abf2aad643644b9697de3611f1
afozbek <furkanozbek1995@gmail.com>
2018-12-08 18:30:05 +0300

    BST_contains

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -28,6 +28,17 @@ class Node {
             this.right = new Node(data);
         }
     }
+    contains(data) {
+        if (data === this.data) {
+            return this;
+        }
+        if (this.data < data && this.right) {
+            return this.right.contains(data);
+        } else if (this.data > data && this.left) {
+            return this.left.contains(data);
+        }
+        return null;
+    }
 }
 
 module.exports = Node;
