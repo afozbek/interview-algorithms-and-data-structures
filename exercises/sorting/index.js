a83544448e72d3848499ec75c139de8da230d3af
afozbek <furkanozbek1995@gmail.com>
2018-12-08 21:35:30 +0300

    merge_sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -32,11 +32,25 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+    if (arr.length === 1)
+        return arr;
 
+    const center = Math.floor(arr.length / 2);
+    const left = arr.slice(0, center);
+    const right = arr.slice(center, arr.length);
+    return merge(mergeSort(left), mergeSort(right))
 }
 
 function merge(left, right) {
-
+    const results = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift());
+        } else {
+            results.push(right.shift());
+        }
+    }
+    return [...results, ...left, ...right];
 }
 
-module.exports = { bubbleSort, selectionSort, mergeSort };
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
