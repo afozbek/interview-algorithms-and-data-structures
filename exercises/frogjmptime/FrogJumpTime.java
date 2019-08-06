e1dac3df7ab31ef5243abc46c7a88962fb3b900f
afozbek <furkanozbek1995@gmail.com>
2019-08-06 14:51:10 +0300

    adding FrogJumpTime algorithm

---
+++ exercises/frogjmptime/FrogJumpTime.java
@@ -0,0 +1,35 @@
+
+/**
+ * Solution
+ */
+import java.util.*;
+
+public class FrogJumpTime {
+
+    public int solution(int X, int[] A) {
+        Set<Integer> leavesSet = new HashSet<Integer>();
+
+        for (int i = 0; i < A.length; i++) {
+            int leaveIndex = A[i];
+
+            if (leaveIndex <= X) {
+                leavesSet.add(A[i]);
+            }
+
+            if (leavesSet.size() == X) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        FrogJumpTime solution = new FrogJumpTime();
+
+        int result = solution.solution(3, new int[] { 1, 3, 1, 4, 1, 3, 5, 4 });
+
+        System.out.println("Your result: " + result);
+
+    }
+}
